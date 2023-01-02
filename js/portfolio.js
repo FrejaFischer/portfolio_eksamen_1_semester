a4ef42ba0472f007703ccffcdc2b2b6cadfae56d
Freja Fischer <frej657e@stud.kea.dk>
2023-01-02 15:31:56 +0100

    indsætter opgaver

--- js/portfolio.js
+++ js/portfolio.js
@@ -2,4 +2,5 @@ window.addEventListener("load", showSite);
 
 function showSite() {
     document.querySelector(".light_box").classList.add("come_out");
+    document.querySelector(".light_box_2").classList.add("come_out_2");
 }
