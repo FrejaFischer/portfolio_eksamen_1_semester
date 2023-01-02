f2e83c97ac6ad0865d0d9e241c6462bbf719df1c
Freja Fischer <frej657e@stud.kea.dk>
2023-01-02 16:06:49 +0100

    løsning til at vise ting ved scroll simpel

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,6 +1,19 @@
 window.addEventListener("load", showSite);
 
 function showSite() {
-    document.querySelector(".light_box").classList.add("come_out");
-    document.querySelector(".light_box_2").classList.add("come_out_2");
+    document.querySelector("#show_1").classList.add("hide");
+    document.querySelector("#show_2").classList.add("hide");
+    document.querySelector("#show_3").classList.add("hide");
+    document.querySelector("#show_4").classList.add("hide");
+    document.querySelector("#show_5").classList.add("hide");
+    document.querySelector("#show_6").classList.add("hide");
+    document.querySelector("#show_7").classList.add("hide");
+
+    document.querySelector(".left_2_1").addEventListener("mouseenter", showImg)
+}
+
+function showImg() {
+    console.log("showImg")
+    document.querySelector("#show_1").classList.remove("hide");
+    document.querySelector("#show_1").classList.add("come_out");
 }
