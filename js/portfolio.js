27480c0d278c65149085ceb5f971fdd5178a49e7
Freja Fischer <frej657e@stud.kea.dk>
2023-01-03 15:40:38 +0100

    skyer, mere tekst you know

--- js/portfolio.js
+++ js/portfolio.js
@@ -2,6 +2,8 @@ window.addEventListener("load", showSite);
 
 function showSite() {
     document.querySelector("#cloud_container").classList.add("cloud_fly");
+    document.querySelector("#cloud_container2").classList.add("cloud_fly2");
+    document.querySelector("#cloud_container3").classList.add("cloud_fly2", "cloud_delay");
 
     document.querySelector("#show_1").classList.add("come_out");
     document.querySelector("#show_3").classList.add("come_out");
