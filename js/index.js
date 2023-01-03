988caf5b1516de8a82eb246897dddacc53c9d6db
Freja Fischer <frej657e@stud.kea.dk>
2023-01-03 10:08:31 +0100

    knap pulser på index yay

---
+++ js/index.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", buttonPulse);
+
+function buttonPulse() {
+  console.log("buttonPulse");
+//   document.querySelector("#button_sprite").classList.add(".pulse");
+//   document.querySelector("#hej").classList.add(".pulse");
+document.querySelector("#button_sprite").classList.add("pulse");
+}
